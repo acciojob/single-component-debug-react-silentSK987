1471c15822bb8beaffe06146537aeeacfa4dae36
Pravin Bansod <136956809+silentSK987@users.noreply.github.com>
2024-03-15 08:11:17 +0000

    ha

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 
-function Para() {
+function App() {
     return (<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>)  
 }
 
-export Para default ;
+export App default ;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Para from "./components/App.js";
+import App from "./components/App.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <div>
-  <Para/>
+  <App/>
   </div>
 ); 
 
